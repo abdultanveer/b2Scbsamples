31fb96264f53d5d7a641b711fb5f54e9c5107e3a
Abdul <abdul.tanveer@gmail.com>
2023-10-25 15:43:47 +0530

    dice roll app

--- app/src/main/java/com/example/b2scbsamples/kotlinexs/Demo.kt
+++ app/src/main/java/com/example/b2scbsamples/kotlinexs/Demo.kt
@@ -7,14 +7,18 @@ class Demo {
 }
 
 fun main() {
-    //Dice myDice = new Dice();
-    val normalDice = Dice(6)
-    val myDice = Dice(20)  //creating dice
-    println("no of sides in this dice is ${myDice.sides}")
+    val myFirstDice = Dice(6)
+    val rollResult = myFirstDice.roll()
+    val luckyNumber = 4
 
-   val uGot = myDice.roll()
-    println("Your ${myDice.sides} sided dice rolled ${uGot}!")
 
-    //var myStudent = Student("abdul",123,true)
 
+    when (rollResult) {
+        luckyNumber -> println("You won!")
+        1 -> println("So sorry! You rolled a 1. Try again!")
+        2 -> println("Sadly, you rolled a 2. Try again!")
+        3 -> println("Unfortunately, you rolled a 3. Try again!")
+        5 -> println("Don't cry! You rolled a 5. Try again!")
+        6 -> println("Apologies! You rolled a 6. Try again!")
+    }
 }
